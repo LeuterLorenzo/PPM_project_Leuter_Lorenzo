54dac7696bdcc642f68879bebcc8777a74cd84b4
Lorenzo Leuter <lorenzo.leuter.stud@unifi.it>
2024-06-19 23:31:52 +0200

    Update for deploy on Render

--- PPM_progect_Leuter_Lorenzo/settings.py
+++ PPM_progect_Leuter_Lorenzo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vfvcel*sm!acv)p+r=h%ie5sr@qp&6814zov@s#!#k++=m=esc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
