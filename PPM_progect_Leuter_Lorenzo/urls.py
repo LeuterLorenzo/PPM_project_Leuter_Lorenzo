398d6c13c76572822678be67c3cc25f70544beb4
Lorenzo Leuter <lorenzo.leuter.stud@unifi.it>
2024-06-21 15:39:16 +0200

    change change-password url and edit base

--- PPM_progect_Leuter_Lorenzo/urls.py
+++ PPM_progect_Leuter_Lorenzo/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('theblog.urls')),
-                  #path('autentification/', include('django.contrib.auth.urls')),
-                  #path('autentification/', include('autentification.urls')),
-                  path('', include("django.contrib.auth.urls")),
-                  path('', include("autentification.urls")),
+                  path('autentification/', include('django.contrib.auth.urls')),
+                  path('autentification/', include('autentification.urls')),
+                  #path('', include("django.contrib.auth.urls")),
+                  #path('', include("autentification.urls")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
